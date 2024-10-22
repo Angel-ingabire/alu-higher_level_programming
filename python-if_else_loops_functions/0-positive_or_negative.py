dfb416bfbaf70f6fc0a94e7b314cd58852a65f3c
Angel-ingabire <a.ingabire2@alustudent.com>
2024-10-22 00:23:04 -0700

    commit

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,14 +1,10 @@
-#!/bin/bash
+#!/usr/bin/python3
 import random
-
-number = random.randint(-10, 10)  # This will assign a random signed number
-
-# Your code starts here
+number = random.randint(-10, 10)
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
-
 
