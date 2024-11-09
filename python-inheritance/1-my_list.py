ed4c84d60845e30305a68c51cb9d2c808f7d1158
Angel-ingabire <a.ingabire2@alustudent.com>
2024-11-09 09:14:45 -0800

    inheitance

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,4 @@
-#!/usr/bin/python3
-# class 'MyList' that inherits from 'list'
-"""
-    define a class 'MyList'
-"""
-
-
+#!/usr/bin/python
 class MyList(list):
-    """
-        implement sorted list
-    """
-
     def print_sorted(self):
-        """
-            prints sorted list
-        """
-        print(sorted(self))
+         print(sorted(self))
