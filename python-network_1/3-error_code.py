b0f3536c3905bc1de19c3e6d8b5d7e1ae7d0520a
Angel-ingabire <a.ingabire2@alustudent.com>
2024-11-20 01:31:15 -0800

    commit

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-# python script that takes in a URL and an email,
-# sends a POST request to the passed URL with the email as a parameter,
-# and displays the body of the response (decoded in utf-8)
+# python script that takes in a URL,
+# sends a request to the URL and displays the body of the response
+# (decoded in utf-8)
 """
-    send a POST request to the passed URL with the email as a parameter,
-    & display body of the response (decoded in utf-8)
+    send a request to the URL & display body of the response
 """
 import sys
-import urllib.parse
+import urllib.error
 import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
